5a557c56cd68715fdbf757568cdc77f789f3fc8d
Charles Martinez <gat-x131@Charless-MacBook-Pro.local>
2023-11-04 12:06:08 +0000

    Fixed temperature conversion

--- TempConversion.js
+++ TempConversion.js
@@ -1,15 +1,27 @@
 // JS function to convert temperature and taking user inputs
 
 document.addEventListener("DOMContentLoaded", () => {
-  const input = document.querySelector("input");
-  const btn = document.querySelector("button");
-  const para = document.querySelector("p");
+  const inputCel = document.getElementById("tempCelcius");
+  const btnCel = document.getElementById("buttonCel");
+  const paraCel = document.getElementById("paraCel");
 
-  btn.addEventListener("click", () => {
-    const tempInput = input.value;
-    const tempInteger = Number(tempInput);
-    const convertedTempInteger = tempInteger * 1.8 + 32;
+  const inputFar = document.getElementById("tempFarenheit");
+  const btnFar = document.getElementById("buttonFar");
+  const paraFar = document.getElementById("paraFar");
 
-    para.textContent = convertedTempInteger;
+  btnCel.addEventListener("click", () => {
+    const tempInputCel = inputCel.value;
+    const tempIntegerCel = Number(tempInputCel);
+    const convertedTempIntCel = tempIntegerCel * 1.8 + 32;
+
+    paraCel.textContent = convertedTempIntCel;
+  });
+
+  btnFar.addEventListener("click", () => {
+    const tempInputFar = inputFar.value;
+    const tempIntegerFar = Number(tempInputFar);
+    const convertedTempIntFar = ((tempIntegerFar - 32) * 5) / 9;
+
+    paraFar.textContent = convertedTempIntFar;
   });
 });
